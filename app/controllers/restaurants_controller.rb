64047584a2fb037f24c3fd2f9430be921b8dd067
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-17 17:31:50 -0500

    Added form validations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,10 +12,14 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
 
-    # redirect to newly created restaurant page
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      # redirect to newly created restaurant page
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+
   end
 
   def show
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :rating, inclusion: { in: [1..10], allow_nil: false }
+  validates :content, presence: true
 end
