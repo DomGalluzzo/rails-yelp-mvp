a27610019ba353622b22a6b06e0fc157fef77db9
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-17 14:23:54 -0500

    restaurants and reviews updated with proper routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# mcdonalds = Restaurant.new(name: 'McDonalds', address: '123 ABC', category: 'chinese', phone_number: '1232123123')
+# mcdonalds_review = Review.new(content: 'nice', rating: 5)
+# mcdonalds_review.restaurant = mcdonalds
+# mcdonalds.save
+
+# chick_fil_a = Restaurant.new(name: 'Chick-fil-A', address: '456 abc', category: 'french', phone_number: '13123123123')
+# chickfila_review = Review.new(content: 'Cool', rating: 10)
+# chickfila_review.restaurant = chick_fil_a
+# chick_fil_a.save
+
+# red_robin = Restaurant.new(name: 'Red Robin', address: '789 ABC', category: 'italian', phone_number: '123213')
+# redrobin_review = Review.new(content: 'okay', rating: 3)
+# redrobin_review.restaurant = red_robin
+# red_robin.save
+
+# burger_king = Restaurant.new(name: 'Burger King', address: '14234 ABC', category: 'belgian', phone_number: '1232123')
+# burgerking_review = Review.new(content: 'nice', rating: 5)
+# burgerking_review.restaurant = burger_king
+# burger_king.save
+
+# wendys = Restaurant.new(name: 'Wendys', address: '765', category: 'chinese', phone_number: '123123123')
+# wendys_review = Review.new(content: 'nice', rating: 5)
+# wendys_review.restaurant = wendys
+# wendys.save
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+# puts "Creating restaurants..."
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: 'italian' }
+# pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: 'chinese'}
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+# puts "Finished!"
